b4a706260b385204fdfb601d3c8d3af7d18785b0
mahbub0074 <mmahbub.cs@gmail.com>
2020-05-07 13:32:18 +0600

    true vs false

---
+++ true-false.js
@@ -0,0 +1,20 @@
+//Falsy
+//false
+//0
+//""
+//undefined
+//null
+//NaN
+//Truthy
+//'0', ' ', []
+
+let name = 0 ;
+//console.log(name);
+if(name || name == 0){
+
+    console.log("condition is true");
+}
+
+else{
+    console.log("condition is false");
+}
