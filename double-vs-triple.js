3b042f96f376b5846e9ba671ec0637a3e06bcc8e
mahbub0074 <mmahbub.cs@gmail.com>
2020-05-08 12:41:17 +0600

    onek kichu siklam

---
+++ double-vs-triple.js
@@ -0,0 +1,23 @@
+// const first = 0;
+// //const second = "2";
+// const second = false;
+
+
+
+// if(first === second){
+//     console.log("condition  is true");
+// }
+// else
+// {
+//     console.log("condition is false");
+// }
+
+
+
+if (‘2’===2){
+    console.log(“Inside if”);
+  }
+  
+  else{
+    console.log(“Inside else”);
+  }
